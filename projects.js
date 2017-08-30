d7171dfb3b16ac3d506fa20bd57c3c35cc3dc87c
Morgan LaCouture <morganlea18@gmail.com>
2017-08-30 16:23:12 -0700

    added more projects to my array of objects

---
+++ projects.js
@@ -0,0 +1,28 @@
+'use strict';
+var projectArr = [
+    {
+        title = 'BusMall',
+        description = 'Simulated Magazine Type Dealio',
+        author = 'Morgan LaCouture',   
+        dateFin =''     
+    },
+    {
+        title = 'Salmon Cookies',
+        description = 'Simulated Cookie Store', 
+        author = 'Morgan LaCouture',
+        dateFin = ''
+    },
+    {
+        title = 'About Me',
+        description = '',
+        author = 'Morgan LaCouture',
+        dateFin = ''
+    },
+    {
+        title = 'Beer Buddy',
+        description = 'Helping the Average Joes Find Their Favorite Beer Style',
+        author = 'David, Eli, Frank Seamen, Morgan LaCouture',
+        dateFin = ''
+    }
+
+]
---
+++ script.js
@@ -0,0 +1,7 @@
+function myProject (title, description, author, date){
+    this.title = title; //name of the project//
+    this.description = description; //tiny description?? not crazy about this one//
+    this.author = author; //typically it would just be me, but i want to showcase BeerBuddy which has numerous authors//
+    this.date = date; //date is was completed//
+    //want to have something here if the project is still being worked on//
+}
