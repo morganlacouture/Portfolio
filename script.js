d7cc953274ad4459aa19b8ab084c14ca2fb9d837
Morgan LaCouture <morganlea18@gmail.com>
2017-09-04 11:52:19 -0700

    converted my array of objects to a json file

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function myProject (title, description, author, date){
+function myProject (title, description, author, dateFin){
     this.title = title; //name of the project//
     this.description = description; //tiny description?? not crazy about this one//
     this.author = author; //typically it would just be me, but i want to showcase BeerBuddy which has numerous authors//
-    this.date = date; //date is was completed//
+    this.dateFin = dateFin; //date is was completed//
     //want to have something here if the project is still being worked on//
+//want to have the author be 'optional' only if theres more authors than just me//
 }
